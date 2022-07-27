66c8ceece12bfac2912f71b885ef725775e3e5f8
Rauchendraser <andreasr@ppedv.de>
2022-07-27 11:01:22 +0200

    Indexvarianten

--- SQL Programmierung/SQL Programmierung/11_Indizes.sql
+++ SQL Programmierung/SQL Programmierung/11_Indizes.sql
@@ -32,8 +32,14 @@ ca 1000 NON CL IX pro Tabelle anlegen
 -----------------------------
 eindeutiger IX
 zusammengesetzter IX
+max 16 Spalten enthalten / max 900bytes
+
 gefilterter IX
 IX mit eingeschlossenen SPalten
+der Baum wird nur mit der Suchspalte gefüllt, aber am Ende des Baumes haben wir sehr viele Were (Evtl alle, weil 1000 Sp möglich)
+--Schlüsselspalte = where
+
+
 abdeckender IX
 indizierte Sicht
 part IX
@@ -57,4 +63,64 @@ select * from best
 --andere Inidzes
 
 select newid()
+
+set statistics io, time on
+--PLAN
+select id from kundeumsatz where id = 10 --TABLE SCAN
+
+--besser mit:  NON CL IX ID --CL IX auf Orderdate vorreserviert
+select id from kundeumsatz where id = 10  --  NON CL IX SEEK --3 Seiten 0 Sek
+
+select id, freight from kundeumsatz where id = 10 --IX Seek mit Lookup .. 4 Seiten
+
+
+select id, freight from kundeumsatz where id < 100 --IX Seek mit Lookup ..Seiten
+
+
+select id, city, country, lastname, freight
+from kundeumsatz
+where freight < 10
+
+
+--
+
+
+-- 102 Seiten Lookup 99% + IX Seek
+
+-- ab ca 12250 DS Wechsel von Seek zu Scan
+select id, freight from kundeumsatz where id < 11000
+--47764 --
+
+--das muss besser gehen: der Lookup muss weg!!!
+--besser mit NIX_ID_FR
+
+select id, freight from kundeumsatz where id < 900000--auch hier noch  reiner IX Seek
+
+--Land dazu --Lookup.. ab 12250 Scan
+select id, freight, country from kundeumsatz where id < 12000
+
+--* nicht besonders gut
+
+
+--eingeschlossene Spalten  SELECT
+
+--NIX_Pid_incl_CYCIUPQU
+select country, city, sum(unitprice*quantity)
+from kundeumsatz
+where  productid < 3
+group by country, city
+
+--eigtl 2 Indizes aber SQL schlägtnmnix mehr vor
+select country, city, sum(unitprice*quantity)
+from kundeumsatz
+where  productid < 3 or freight < 1
+group by country, city
+
+
+-- in so einem Fall: 
+--immer Klammern setzen.. das AND wird immer zuerst gebunden
+select country, city, sum(unitprice*quantity)
+from kundeumsatz
+where  productid < 3 or freight < 1 and employeeid = 2
+group by country, city
 
